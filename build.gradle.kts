08f9a417f95ac8650ce1516644a0ecb8117233fa
kof99athena <kof99athena@gmail.com>
2023-09-04 00:47:42 +0900

    update 1 : hilt라이브러리 추가 및 jvmTarget 수정 (1.8에서 17로)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,10 @@
 plugins {
     alias(libs.plugins.com.android.application) apply false
     alias(libs.plugins.org.jetbrains.kotlin.android) apply false
+
+    //1. 힐트 라이브러리 추가하자
+    id("com.google.dagger.hilt.android") version "2.44.2" apply false
+    //id 'com.android.library' version '7.3.1' apply false
+    id ("com.android.library") version "7.3.1" apply false
+
 }
